d8f4545bf67ad0ad27906cdf5ffeebcba40c0b3b
Leonie <leonie.reif@gatech.edu>
2016-10-16 17:50:11 -0400

    new test

--- index.js
+++ index.js
@@ -6,10 +6,10 @@ app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
 // views is directory for all template files
-/*app.set('views', __dirname + '/views');
+app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
-app.get('/', function(request, response) {
+/*app.get('/', function(request, response) {
   response.render('pages/index');
 });
 
