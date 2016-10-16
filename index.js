469830020967c4024ed38a59658d204f495efaaa
leoniereif <leonie.reif@gatech.edu>
2016-10-16 18:05:32 -0400

    adding postgres

--- index.js
+++ index.js
@@ -17,5 +17,16 @@ app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
 
+var pg = require('pg');
 
-
+app.get('/db', function (request, response) {
+  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+    client.query('SELECT * FROM test_table', function(err, result) {
+      done();
+      if (err)
+       { console.error(err); response.send("Error " + err); }
+      else
+       { response.render('pages/db', {results: result.rows} ); }
+    });
+  });
+});
