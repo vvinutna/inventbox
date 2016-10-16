cdb29d7e990bdc157d2b2713423ff27bebd71fad
Leonie <leonie.reif@gatech.edu>
2016-10-16 17:52:26 -0400

    back to normal

--- index.js
+++ index.js
@@ -10,10 +10,8 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
-  //response.render('pages/index');
+  response.render('pages/index');
 });
-
-response.end('Hello World\n');
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
