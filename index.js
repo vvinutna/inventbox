4961e756b6adf90f696e3d3068ed0ba75cd3e732
leoniereif <leonie.reif@gatech.edu>
2016-11-19 20:17:54 -0500

    changed database path request in index.js

--- index.js
+++ index.js
@@ -19,14 +19,14 @@ app.listen(app.get('port'), function() {
 
 var pg = require('pg');
 
-app.get('/db', function (request, response) {
+app.get('/', function (request, response) {
   pg.connect(process.env.DATABASE_URL, function(err, client, done) {
     client.query('SELECT * FROM test_table', function(err, result) {
       done();
       if (err)
        { console.error(err); response.send("Error " + err); }
       else
-       { response.render('pages/db', {results: result.rows} ); }
+       { response.render('pages/index', {results: result.rows} ); }
     });
   });
 });
