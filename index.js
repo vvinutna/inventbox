65f7462314078b114f27fae13eab699741a9a343
leoniereif <leonie.reif@gatech.edu>
2016-11-19 20:23:46 -0500

    removed second db connection from index.js

--- index.js
+++ index.js
@@ -30,14 +30,3 @@ app.get('/db', function (request, response) {
     });
   });
 });
-app.get('/', function (request, response) {
-  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-    client.query('SELECT * FROM test_table', function(err, result) {
-      done();
-      if (err)
-       { console.error(err); response.send("Error " + err); }
-      else
-       { response.render('pages/index', {results: result.rows} ); }
-    });
-  });
-});
