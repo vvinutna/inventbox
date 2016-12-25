63b98408423c82b012ae3cd5afa8d63c51158d7d
Leonie <leonie.reif@gatech.edu>
2016-12-25 17:15:46 +0100

    rendering add page

--- index.js
+++ index.js
@@ -13,6 +13,10 @@ app.get('/', function(request, response) {
   response.render('pages/index');
 });
 
+app.get('/add', function(request, response) {
+  response.render('pages/addItems');
+});
+
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
