41dbd95f89e4a9966c68fdbecfddbb4b058bcafb
Leonie <leonie.reif@gatech.edu>
2016-12-25 17:38:09 +0100

    database connection for index

--- index.js
+++ index.js
@@ -10,7 +10,15 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
-  response.render('pages/index');
+  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+    client.query('SELECT * FROM test_table', function(err, result) {
+      done();
+      if (err)
+       { console.error(err); response.send("Error " + err); }
+      else
+       { response.render('pages/index', {results: result.rows} ); }
+    });
+  });
 });
 
 app.get('/addItems', function(request, response) {
