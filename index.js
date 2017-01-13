0944d942d717a74dd29643770b6f16b8668e6fcd
vvinutna <vinutna.veeragandham@gmail.com>
2017-01-12 20:11:56 -0500

    Update index.js

--- index.js
+++ index.js
@@ -17,16 +17,20 @@ app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
+// app.get('/', function(request, response) {
+//     pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+//       client.query('SELECT * FROM test_table', function(err, result) {
+//         done();
+//         if (err)
+//          { console.error(err); response.send("Error " + err); }
+//         else
+//          { response.render('pages/dashboard', {results: result.rows} ); }
+//       });
+//     });
+// });
+
 app.get('/', function(request, response) {
-    pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-      client.query('SELECT * FROM test_table', function(err, result) {
-        done();
-        if (err)
-         { console.error(err); response.send("Error " + err); }
-        else
-         { response.render('pages/dashboard', {results: result.rows} ); }
-      });
-    });
+    response.render('pages/index');
 });
 
 app.get('/login', function(request, response) {
