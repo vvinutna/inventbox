ab52913b4c089e71faf447b622bfef055bf26d26
vvinutna <vinutna.veeragandham@gmail.com>
2017-02-22 20:55:43 -0500

    update itemcategories

--- index.js
+++ index.js
@@ -114,7 +114,7 @@ app.get('/updateItemsStock', function(request, response) {
       return res.status(500).json({success: false, data: err});
     }
     // SQL Query > Select Data
-    const query = client.query('SELECT * FROM products;');
+    const query = client.query('SELECT * FROM categories;');
     // Stream results back one row at a time
     query.on('row', (row) => {
       items.push(row);
